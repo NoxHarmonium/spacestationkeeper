e75de7cee45de4ceeb0bd5760377231d2eefd498
Sean Dawson <sean@two-bulls.com>
2014-04-20 21:28:38 -0400

    Added liblua in preperation for luabind. Moved some header files around.

---
+++ include/Binding/LuaBridge.h
@@ -0,0 +1,16 @@
+//
+//  LuaBridge.h
+//  SpaceStationKeeper
+//
+//  Created by Sean Dawson on 20/04/2014.
+//
+//
+
+#ifndef __SpaceStationKeeper__LuaBridge__
+#define __SpaceStationKeeper__LuaBridge__
+
+#include <iostream>
+
+// Can't do anything until I get internet to download bjam
+
+#endif /* defined(__SpaceStationKeeper__LuaBridge__) */
---
+++ src/Binding/LuaBridge.cpp
@@ -0,0 +1,11 @@
+//
+//  LuaBridge.cpp
+//  SpaceStationKeeper
+//
+//  Created by Sean Dawson on 20/04/2014.
+//
+//
+
+#include "LuaBridge.h"
+
+// Can't do anything until I get internet to download bjam
